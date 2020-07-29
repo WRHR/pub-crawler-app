97636faab1ecb2f34f6704339396563bfefa8eef
WRHR <wrhreeves@gmail.com>
2020-07-29 11:09:27 -0600

    Create nav bar. Add logout, Add rediects after log in and out, authorize user on login. User persists in state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Login from './components/Login';
+import {Route, Switch, Redirect, Link} from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute';
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    user:{}
+  }
+
+  componentDidMount(){
+    if(localStorage.token){
+      this.authorizeUser()
+    }
+  }
+
+  setUser = (user) => this.setState({user: user})
+
+  authorizeUser = () => {
+    fetch("http://localhost:3000/profile", {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${localStorage.token}`
+      }
+    })
+    .then(response => response.json())
+    .then(result => {
+      this.setState({
+        user: result.user
+      })
+    })
+  }
+
+  render(){
+    const {user} = this.state
+    return (
+      <div className="App">
+        <header>
+          {user.username 
+            ? (
+              <nav>
+                <h3>Pub Crawler</h3>
+                <Link to='/login'>Logout</Link>
+              </nav>
+            )
+            : null
+          }
+        </header>
+        <Switch>
+          <PrivateRoute 
+            exact
+            path='/'
+            component={Home}
+          />
+          <Route exact path='/login' render={(routerProps) => {return <Login setUser={this.setUser} {...routerProps} />} }/>
+          <Redirect to='/' />
+        </Switch>
+        
+      </div>
+    );
+  }
+    
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react'
+
+export default class Login extends Component {
+    state = {
+        needsLogin: false,
+        formUrl: 'http://localhost:3000/login',
+        username: '',
+        password: '',
+        submitButtonValue: 'Login',
+        alerts: []
+    }
+
+    componentDidMount(){
+        localStorage.removeItem('token')
+        this.props.setUser({})
+    }
+
+    login = () => {
+        this.setState({
+            formUrl: 'http://localhost:3000/login',
+            submitButtonValue: 'Login',
+            alerts: []
+        })
+    }
+
+    signUp = () => {
+        this.setState({
+            formUrl: 'http://localhost:3000/users/',
+            submitButtonValue:'Sign Up',
+            alerts:[]
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleClick= (event) => {
+        event.preventDefault()
+        this.setState({needsLogin: !this.state.needsLogin})
+        this.state.needsLogin ? this.login() :this.signUp()  
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const {username, password} = this.state
+        let user = {username, password}
+
+        fetch(this.state.formUrl, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify({user})
+        })
+            .then(response => response.json())
+            .then(results => {
+                if(results.errors){
+                    this.setState({alerts: results.errors})
+                } else {
+                    localStorage.setItem('token', results.token)
+                    this.props.setUser(results.user)
+                }
+            })
+            .then(() => this.props.history.push('/'))
+    }
+
+    showAlerts = () => this.state.alerts
+        .map((alert, index) => <p className='alert' key={index}>{alert}</p>)
+    
+
+    render(){
+        const {username, password, submitButtonValue, alerts, needsLogin} = this.state
+        return(
+            <div className='form-container'>
+                <h1 className='logo'>Pub Crawler</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <h2>{needsLogin ? "Sign Up" : "Log In"}</h2>
+                    <label>Username:</label>
+                    <input name='username' value={username} onChange={this.handleChange}/>
+                    <label>Password:</label>
+                    <input name='password' value={password} onChange={this.handleChange}/>
+                    {alerts ? this.showAlerts() : null}
+                    <input className='login-buttons' type='submit' value={submitButtonValue}/>
+                    <p>or</p>
+                    <button className='login-buttons' onClick={this.handleClick}>
+                        {needsLogin ? "Back to Log In" : "Sign Up New User"}
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
